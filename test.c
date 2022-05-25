8208f0f6f249b4354a10b8896feb17f1ece2e812
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-05-25 14:31:42 +0800

    more test suits.

--- test.c
+++ test.c
@@ -31,6 +31,8 @@ void node_walk_cb2(struct bst_node* x, void* p) {
 }
 
 void test_bst2(void);
+void test_bst_string(void);
+void test_bst_string2(void);
 
 int main() {
     struct bst_tree* tree = bst_tree_create(int_ptr_compare, NULL);
@@ -68,6 +70,8 @@ int main() {
     bst_destroy_tree(tree);
 
     test_bst2();
+    test_bst_string();
+    test_bst_string2();
 
     return 0;
 }
@@ -100,6 +104,82 @@ void test_bst2(void)
 
     bst_inorder_walk(t, node_walk_cb2, NULL);
     printf("\n\n");
+
+    bst_destroy_tree(t);
+}
+
+int string_ptr_compare(const void* lhs, const void* rhs) {
+    char *left = *(char**)lhs;
+    char *right = *(char**)rhs;
+    return strcmp(left, right);
+}
+
+void string_ptr_destroy(void*p) {
+    char* _p = *(char**)p;
+    free(_p);
+}
+
+void node_walk_cb3(struct bst_node* node, void* p) {
+    char *str = *(char**)bst_node_get_key(node);
+    printf("%s \n", str);
+    (void)p;
+}
+
+char* strArr[] = {
+    "abcd1234",
+    "good_idea",
+    "just",
+    "ding",
+};
+
+void test_bst_string(void) {
+    const struct bst_node* node;
+    size_t i;
+    struct bst_tree* t = bst_tree_create(string_ptr_compare, string_ptr_destroy);
+    for (i = 0; i < sizeof(strArr)/sizeof(strArr[0]); i++) {
+        char* y;
+        char* p = strdup(strArr[i]);
+        if (0 != bst_insert(t, &p, sizeof(p))) {
+            continue;
+        }
+        node = bst_find_node(t, &strArr[i]);
+        y = *(char**)bst_node_get_key(node);
+        assert(strcmp(y, strArr[i]) == 0);
+    }
+
+    printf("\n==== test_bst_string ====\n");
+    bst_inorder_walk(t, node_walk_cb3, NULL);
+
+    bst_destroy_tree(t);
+}
+
+int string_ptr_compare2(const void* lhs, const void* rhs) {
+    return strcmp((char*)lhs, (char*)rhs);
+}
+
+void node_walk_cb4(struct bst_node* node, void* p) {
+    char* str = (char*)bst_node_get_key(node);
+    printf("%s \n", str);
+    (void)p;
+}
+
+void test_bst_string2(void) {
+    const struct bst_node* node;
+    size_t i;
+    struct bst_tree* t = bst_tree_create(string_ptr_compare2, NULL);
+    for (i = 0; i < sizeof(strArr) / sizeof(strArr[0]); i++) {
+        char* y;
+        char* p = strArr[i];
+        if (0 != bst_insert(t, p, strlen(p)+1)) {
+            continue;
+        }
+        node = bst_find_node(t, strArr[i]);
+        y = (char*)bst_node_get_key(node);
+        assert(strcmp(y, strArr[i]) == 0);
+    }
+
+    printf("\n==== test_bst_string2 ====\n");
+    bst_inorder_walk(t, node_walk_cb4, NULL);
 
     bst_destroy_tree(t);
 }
